798aa1f616ab52505cc7e077aa73bb028b997664
mmanry <mmanry@etu.isara.fr>
2021-05-04 11:30:21 +0200

    le serveur pour le pdf

--- server.R
+++ server.R
@@ -6,13 +6,9 @@ shinyServer(function(input, output) {
     #output$distPlot <- renderPlot({
 
     output$report <- downloadHandler(
-        # For PDF output, change this to "report.pdf"
-        filename = "report.html",
+        filename = "report.pdf",
         content = function(file) {
             
-            # Copy the report file to a temporary directory before processing it, in
-            # case we don't have write permissions to the current working dir (which
-            # can happen when deployed).
             tempReport <- file.path(tempdir(), "report.Rmd")
             file.copy("report.Rmd", tempReport, overwrite = TRUE)
             
