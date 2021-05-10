cbe29ca42bc97d35da6b7d08a2c1cab563dc94c9
hfavet <hfavet@etu.isara.fr>
2021-05-10 10:43:26 +0200

    ajout date mise à jour et recette

--- ui.R
+++ ui.R
@@ -7,16 +7,18 @@ shinyUI(fluidPage(
     titlePanel("Nutrisara"),
 
     # Sidebar with a slider input for number of bins----
-    sidebarLayout(
-        sidebarPanel(
-            textInput("nompre", label = "Nom prenom operateur", value = "", width=NULL, placeholder=NULL),
-            dateInput(inputId = "idDateCre", label = "Date de creation", value = NULL,
-                      format = "dd/mm/yyyy", startview = "decade", weekstart = 0, language = "fr"),
-            downloadButton("report", "Generate report")
+    sidebarPanel(
+        textInput("nompre", label = "Nom prenom operateur", value = "", width=NULL, placeholder=NULL),
+        dateInput(inputId = "idDateCre", label = "Date de creation", value = NULL,
+                  format = "dd/mm/yyyy", startview = "decade", weekstart = 0, language = "fr"),
+        dateInput(inputId = "IDdate", label = "Date de mise a jour", value = NULL,
+                  format = "dd/mm/yyyy", startview = "decade", weekstart = 0, language = "fr"),
+        textInput("idrecette", label = "Recette", value = "", width=NULL, placeholder=NULL),
+        downloadButton("report", "Generate report")
 ),
         # Show a plot of the generated distribution----
         mainPanel(
             #plotOutput()
         )
     )
-))
+)
