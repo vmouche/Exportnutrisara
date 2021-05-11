9d904fb9fb84e9252ba78790dc4ee81d82bbf33e
mmanry <mmanry@etu.isara.fr>
2021-05-11 14:36:26 +0200

    code pour graph des desserts

--- ui.R
+++ ui.R
@@ -1,11 +1,11 @@
 library(shiny)
 library(readxl)
 library(tidyr)
-#tabtest <- read_excel("Tabtestc.xls")
-#tabtest<- read.csv(Tabtestc.csv)
-#newtab<- pivot_longer(tabtest, c("Eau", "Proteines", "Glucides", "Lipides", "Sucres", "FibresAlimentaires", "AGsat", "Sel", "Autre", "total"), names_to = "class", values_to = "pourcentage")
-tabtest2<-read.csv2("Tabledonnee2.csv", header=TRUE, sep=";")
-newtab2<-pivot_longer(tabtest2,c("Eau","Proteines", "Glucides", "Lipides", "Sucres", "FibresAlimentaires", "AG_satures","Sel"))
+
+tabtest2<-read.csv2("Tabtest.csv", header=TRUE, dec=",", sep=";", encoding = "latin1")
+newtab2<-pivot_longer(tabtest2,c("Eau","Protéines", "Glucides", "Lipides", "Sucres", "Fibres.alimentaires", "AG.saturés","Sel"))
+colnames(newtab2)<-c("nomr", "name", "value")
+#write.csv2(newtab2, file="newtab2.csv")
 newtab2
 
 
@@ -16,16 +16,19 @@ shinyUI(fluidPage(
     titlePanel("Nutrisara"),
 
     # Sidebar with a slider input for number of bins----
+
     sidebarPanel(
-        textInput("nompre", label = "Nom prenom operateur", value = "", width=NULL, placeholder=NULL),
-        dateInput(inputId = "idDateCre", label = "Date de creation", value = NULL,
+            textInput("nompre", label = "Nom prenom operateur", value = "", width=NULL, placeholder=NULL),
+            dateInput(inputId = "idDateCre", label = "Date de creation", value = NULL,
                   format = "dd/mm/yyyy", startview = "decade", weekstart = 0, language = "fr"),
-        dateInput(inputId = "IDdate", label = "Date de mise a jour", value = NULL,
+            dateInput(inputId = "IDdate", label = "Date de mise a jour", value = NULL,
                   format = "dd/mm/yyyy", startview = "decade", weekstart = 0, language = "fr"),
-        textInput("idrecette", label = "Recette", value = "", width=NULL, placeholder=NULL),
+            textInput("idrecette", label = "Recette", value = "", width=NULL, placeholder=NULL),
         #
-        selectInput("nom", "ingredient",choices = c(tabtest2$nom)),
-        mainPanel(plotOutput("piePlot")),
+            selectInput("nomI", "ingredient",choices = c(newtab2$nomr)),
+            ),
+            
+    mainPanel(plotOutput("scatterPlot")),
         #
         downloadButton("report", "Generate report"),
         # Show a plot of the generated distribution----
@@ -33,4 +36,4 @@ shinyUI(fluidPage(
             
         )
     )
-)
+
